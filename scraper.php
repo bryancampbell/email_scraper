8bc450890f60958d273e4454050b0e96f43aac4e
bryancampbell <bryan@familycampbell.org>
2020-07-28 00:04:11 -0400

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,16 +2,20 @@
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+
+// Read in a page
+$html = scraperwiki::scrape("https://www.southfieldk12.org/departments/administration/");
+
+
+$xml=simplexml_load_string($html);
+print_r($xml);
+
+// Find something on the page using css selectors
+//$dom = new simple_html_dom();
+//$dom->load($html);
+//print_r($dom->find("table.list"));
 //
 // // Write out to the sqlite database using scraperwiki library
 // scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
